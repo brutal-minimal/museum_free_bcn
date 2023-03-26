7a4c0f7084c85e777243e54e63eb3ce34351d6bb
Yulia Slobodeniuk <gh732@yahoo.com>
2023-03-26 16:32:48 +0200

    Map added to a Monday page.

--- lib/monday.dart
+++ lib/monday.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'menu.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
 class Monday extends StatefulWidget {
   const Monday({super.key});
 
@@ -19,11 +22,32 @@ class _MondayState extends State<Monday> {
             const Text("Like Museu d`Història de Barcelona (MUHBA)"),
             const Text("15-20"),
             const Text("Plaza del Rey, s/n, 08002 ,M Jaume I "),
-            Image.asset(scale: 5.0,"images/pinguilove.png")
+            Image.asset(scale: 5.0,"images/pinguilove.png"),
+            Container(
+              width: 300,
+              height: 200,
+              child: FlutterMap(
+                options: MapOptions(
+                center:  LatLng(51.509364, -0.128928),
+                zoom: 9.2),
+                nonRotatedChildren: [
+                    AttributionWidget.defaultWidget(
+              source: 'OpenStreetMap contributors',
+              onSourceTapped: null,
+                    ),
+                ],
+                children: [
+                    TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+                    ),
+                ],),
+            )
           ],
         ),),
       appBar: AppBar(
         title:  Text(weekdays[0]),
-      ),);
+      ),
+      );
   }
 }
